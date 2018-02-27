5466259c410ae03bda5aca6c0ce2a3a66996b7ca
jameson <jamesonathanasiou@gmail.com>
2018-02-27 17:25:45 -0500

    nvm, using jest. also cleaning up dependencies

--- src/requestor/__tests__/powerRankingsRequestor.test.js
+++ src/requestor/__tests__/powerRankingsRequestor.test.js
@@ -1,29 +1,8 @@
-import assert from 'assert';
-import jsdom from 'mocha-jsdom';
-import sinon from 'sinon';
 import powerRankingsRequestor from '../powerRankingsRequestor';
 
-const sandbox = sinon.createSandbox();
-const stubs = {};
+test('When posting power rankings then the endpoint is correct', () => {
+    window.fetch = jest.fn();
 
-const theDom = new jsdom('<div></div>');
-const { window } = theDom;
-global.window = window;
-
-describe('Array', function() {
-    beforeEach(()=> {
-
-    });
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
-
-        it('When posting then the endpoint is /powerRankings', () => {
-            stubs.fetch = sandbox.stub(window, 'fetch');
-            powerRankingsRequestor.post();
-            assert.equal(fetch.args[0][0], '/powerRankings');
-        });
-
-    });
+    powerRankingsRequestor.post();
+    expect(window.fetch).toHaveBeenCalledWith('/powerRankings', expect.anything());
 });
