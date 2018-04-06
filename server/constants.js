f6315e8a5bbbba329fab74b72231c4340d50b427
jameson <jamesonathanasiou@gmail.com>
2018-04-06 07:48:01 -0400

    start playing around with hidden api

--- server/constants.js
+++ server/constants.js
@@ -4,5 +4,13 @@ module.exports = {
             GAMES: 'Games',
             TEAMS: 'Teams'
         }
+    },
+    API: {
+        ESPN: {
+            HOST: 'https://games.espn.com/ffl/api/v2'
+        }
+    },
+    LEAGUE: {
+        ID: '211640'
     }
 };
--- server/espnAccessor.js
+++ server/espnAccessor.js
@@ -1,5 +1,7 @@
 const espnData = require('espn-fantasy-football-data');
 const dataAccessor = require('./dataAccessor');
+const { API, LEAGUE } = require('./constants');
+const request = require('request');
 
 const getRankings = async (week, position) => {
     const data = await espnData.getPlayerRankings(week, position);
@@ -38,7 +40,27 @@ const getEspnData = async () => new Promise(async (resolve, reject) => {
     }, reject);
 });
 
+const getDataFromApi = () => {
+    // const url = `${API.ESPN.HOST}/scoreboard?leagueId=${LEAGUE.ID}&seasonID=2017&matchupPeriodId=1`;
+    // const url = `${API.ESPN.HOST}/players?playerId=12514`;
+    // const url = 'http://games.espn.com/ffl/api/v2/standings?leagueId=' + leagueId + '&seasonId=2017';
+    // const url = 'http://games.espn.com/ffl/api/v2/boxscore?leagueId=' + leagueId + '&teamId=' + teamId + '&scoringPeriodId=' + scoringPeriodId + '&seasonId=2017';
+    // const url = `https://games.espn.com/ffl/api/v2/leagueSettings?leagueId=${leagueId}&seasonId=${seasonId}`;
+
+    const url = `${API.ESPN.HOST}/rosterInfo?leagueId=${LEAGUE.ID}&seasonID=2017&teamIds=1`;
+    return new Promise((resolve, reject) => {
+        request(url, (err, response, body) => {
+            if (!err) {
+                resolve(body);
+            } else {
+                reject(err);
+            }
+        });
+    });
+};
+
 module.exports = {
+    getDataFromApi,
     getEspnData,
     getRankings
 };
