165dafac985877743b84f5965bbca67e96a85fde
jameson <jamesonathanasiou@gmail.com>
2018-05-26 13:15:25 -0400

    use material table for standings

--- src/components/output/Standings.js
+++ src/components/output/Standings.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import { Avatar, ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, LinearProgress, Typography } from '@material-ui/core';
+import { Avatar, ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, LinearProgress, Typography, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PropTypes from 'prop-types';
+import StandingsDetail from 'components/output/StandingsDetail';
+import { merge } from 'lodash';
+
+const styles = {
+    progress: {
+        'padding-top': '2em',
+        'padding-bottom': '2em'
+    },
+    panelSummary: {
+        display: 'flex',
+        'justify-content': 'center',
+        'align-items': 'center',
+        'padding-left': '10px'
+    }
+};
 
 class Standings extends React.Component {
     state = {
-        standings: [],
+        teamData: [],
         loading: true
     };
 
     componentDidMount() {
-        fetch('/standings').then(data => data.json()).then(({ standings }) => {
+        const getStandings = new Promise((resolve, reject) => {
+            fetch('/standings').then(data => data.json()).then(({ standings }) => {
+                resolve(standings);
+            }, reject);
+        });
+
+        const getStats = new Promise((resolve, reject) => {
+            fetch('/stats').then(data => data.json()).then(({stats}) => {
+                resolve(stats);
+            }, reject);
+        });
+
+        Promise.all([getStandings, getStats]).then((values) => {
+            const standings = values[0];
+            const stats = values[1];
+            const teamData = standings.map((team, index) => {
+                return merge(team, stats[index]);
+            });
+
             this.setState({
-                standings,
+                teamData,
                 loading: false
             });
         });
@@ -21,13 +56,10 @@ class Standings extends React.Component {
         const content = this.state.standings.map((team, index) => (
             <ExpansionPanel key={index + 1} defaultExpanded={!index}>
                 <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Avatar>{index + 1}</Avatar><Typography> {team.team} </Typography>
+                    <Avatar>{index + 1}</Avatar><Typography className={this.props.classes.panelSummary}> {team.team} </Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
-                    <Typography>
-                       wins: {team.wins}
-                       losses: {team.losses}
-                    </Typography>
+                    <StandingsDetail data={this.state.standings[index]} />
                 </ExpansionPanelDetails>
             </ExpansionPanel>
         ));
@@ -35,8 +67,43 @@ class Standings extends React.Component {
         return <div> {content} </div>;
     }
 
+    buildTable() {
+        const table = (
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Rank</TableCell>
+                            <TableCell>Team</TableCell>
+                            <TableCell>Wins</TableCell>
+                            <TableCell>Losses</TableCell>
+                            <TableCell>Games Behind</TableCell>
+                            <TableCell>Points Scored</TableCell>
+                            <TableCell>Points Allowed</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.teamData.map((team, index) => (
+                            <TableRow key={index + 1} >
+                                <TableCell>{index + 1 }</TableCell>
+                                <TableCell>{team.team}</TableCell>
+                                <TableCell>{team.wins}</TableCell>
+                                <TableCell>{team.losses}</TableCell>
+                                <TableCell>{team.gamesBehind}</TableCell>
+                                <TableCell>{team.pointsScored}</TableCell>
+                                <TableCell>{team.pointsAgainst}</TableCell>
+                            </TableRow>
+                        ))};
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+
+        return table;
+    }
+
     getLoadingContent = () => (
-        <div>
+        <div className={this.props.classes.progress}>
             <LinearProgress />
             <br />
             <LinearProgress color="secondary" />
@@ -44,8 +111,12 @@ class Standings extends React.Component {
     );
 
     render() {
-        return this.state.loading ? this.getLoadingContent() : this.buildStandingsPanels();
+        return this.state.loading ? this.getLoadingContent() : this.buildTable();
     }
 }
 
-export default Standings;
+Standings.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Standings);
---
+++ src/components/output/StandingsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableRow, Paper } from '@material-ui/core';
+
+const StandingsDetail = ({ data }) => (
+    <Paper>
+        <Table>
+            <TableBody>
+
+            </TableBody>
+        </Table>
+    </Paper>
+);
+
+export default StandingsDetail;
